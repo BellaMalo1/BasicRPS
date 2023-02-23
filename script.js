ab6aeb6c62a07a8aecf65d15508e66f7dcbc236b
BellaMalo <bmaldo23@gmail.com>
2023-02-23 15:26:54 -0800

    buttons for player added

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//button variables + Listeners 
+const rock = document.querySelector('#Rock')
+const paper = document.querySelector('#Paper')
+const scissors = document.querySelector('#Scissors')
+
+function click (){
+    console.log('clicked')
+}
+
+rock.addEventListener('click', click)
+paper.addEventListener('click', click)
+scissors.addEventListener('click', click)
+
+
+
+
+
 // create objects to store Computer Player rolls
 
 const computer = {
@@ -13,6 +30,17 @@ const rollType = ["Papyrus", "Scalpellus","Lapis"]
 
 const randomIndex = Math.floor(Math.random() * rollType.length)
 
+//Set Computer Object Roll Field to the roll type at the Random Index
+
 computer.currentRoll = rollType[randomIndex];
 
+//Conditionals to keep track of who is winning 
+
 console.log('the computer rolled a ' + computer.currentRoll)
+
+if(computer.currentRoll === "Papyrus"){
+    console.log("match")
+} 
+
+
+
